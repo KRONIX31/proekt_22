e436d48d5c110a1db071a56da1acac9f6894d54a
KR <78811949+KRONIX31@users.noreply.github.com>
2023-01-19 19:35:54 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,27 +1,41 @@
-const staticCacheName = 'static-cache-v0'
+const staticCacheName = 'static-cache-v1'
 const dynamicCacheName = 'dynamic-cache-v0'
 const staticAssets = [
+    '/',
     '/index.html',
     '/icon192.png',
     '/icon512.png',
     '/style.css',
     '/main.js',
     '/app.js',
+    '/manifest.json',
     '/burger.svg',
-    '/close.svg'
+    '/close.svg',
+    '/Meteor_mobile.svg',
+    '/Meteor.svg',
+    '/github_mark_white.svg',
+    'Comic_CAT.otf',
+    'chic.png',
 ]
 self.addEventListener('install', async (e)=>{
     console.log('service Worker установлен', e)
     self.skipWaiting()
-    
-    const cache = await caches.open(staticCacheName)
-    await cache.addAll(staticAssets)
+    e.waitUntil(
+        caches.open(staticCacheName)
+          .then(function(cache) {
+            console.log('Opened cache');
+            return cache.addAll(staticAssets);
+          })
+      )
+    //const cache = await caches.open(staticCacheName)
+    //await cache.addAll(staticAssets)
 })
 self.addEventListener('activate', async (e)=>{
     console.log('service Worker активирован', e)
 
     cachesKeys = await caches.keys()
     const checkKeys = cachesKeys.map(async key =>{
+        console.log(key)
         if(staticCacheName !== key){
             await caches.delete(key)
         }
@@ -29,14 +43,11 @@ self.addEventListener('activate', async (e)=>{
     await Promise.all(checkKeys)
 })
 
-self.addEventListener('fetch', (e)=>{
-    e.respondWith(cacheFirst(e.request))
+self.addEventListener("fetch", function (event) {
+    console.log(event.request)
+    event.respondWith(
+        fetch(event.request).catch(function(){
+            return caches.match(event.request)
+        })
+    )
 })
-async function cacheFirst(request){
-    const cached = await caches.match(request)
-    if(cached){
-        return cached
-    } else{
-        return await fetch(request)
-    }
-}
