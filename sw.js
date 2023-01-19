4a221ff6d983b52035f2931fe2d7702444b20970
KR <78811949+KRONIX31@users.noreply.github.com>
2023-01-19 20:13:58 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,52 @@
+const staticCacheName = 'static-cache-v1'
+const dynamicCacheName = 'dynamic-cache-v0'
+const staticAssets = [
+    '/',
+    '/index.html',
+    '/icon192.png',
+    '/icon512.png',
+    '/style.css',
+    '/main.js',
+    '/app.js',
+    '/manifest.json',
+    '/burger.svg',
+    '/close.svg',
+    '/Meteor_mobile.svg',
+    '/Meteor.svg',
+    '/github_mark_white.svg',
+    'Comic_CAT.otf',
+    'chic.png',
+]
+self.addEventListener('install', async (e)=>{
+    console.log('service Worker установлен', e)
+    self.skipWaiting()
+    e.waitUntil(
+        caches.open(staticCacheName)
+          .then(function(cache) {
+            console.log('Opened cache');
+            return cache.addAll(staticAssets);
+          })
+      )
+    //const cache = await caches.open(staticCacheName)
+    //await cache.addAll(staticAssets)
+})
+self.addEventListener('activate', async (e)=>{
+    console.log('service Worker активирован', e)
+
+    cachesKeys = await caches.keys()
+    const checkKeys = cachesKeys.map(async key =>{
+        console.log(key)
+        if(staticCacheName !== key){
+            await caches.delete(key)
+        }
+    })
+    await Promise.all(checkKeys)
+})
+
+self.addEventListener("fetch", function (event) {
+    event.respondWith(
+        fetch(event.request).catch(function(){
+            return caches.match(event.request)
+        })
+    )
+})
