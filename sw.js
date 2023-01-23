c08afd20c5a3a42846a594d45eba289fa19351f6
KR <78811949+KRONIX31@users.noreply.github.com>
2023-01-23 18:54:06 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const staticCacheName = 'static-cache-v2'
+const staticCacheName = 'static-cache-v4'
 const staticAssets = [
     '.',
     'index.html',
@@ -12,7 +12,9 @@ const staticAssets = [
     'close.svg',
     'Meteor.svg',
     'Meteor_mobile.svg',
-    'Comic_CAT.otf'
+    'Comic_CAT.otf',
+    'github_mark_white.svg',
+    'chic.png',
 ]
 self.addEventListener('install', async (e)=>{
     console.log('service Worker установлен', e)
@@ -30,8 +32,8 @@ self.addEventListener('install', async (e)=>{
         })()
     )*/
     const cache = await caches.open(staticCacheName)
-    staticAssets.forEach(element => {
-        cache.add(element).catch((error)=>{
+    staticAssets.forEach(async element => {
+        await cache.add(element).catch((error)=>{
             console.log(error)
         })
     })
@@ -50,28 +52,30 @@ self.addEventListener('activate', async (e)=>{
     await Promise.all(checkKeys)
 })
 
-self.addEventListener('fetch', (e)=>{
-    /*e.respondWith(
-        fetch(e.request).catch(function(){
-            return caches.match(e.request)
-        })
-    )*/
-    e.respondWith((async ()=>{
-        const response = await fetch(e.request).catch(error => {
-            console.warn(error)
-        })
-        if(response){
-            console.log('Internet', response)
-            return response
-        } else{
-            caches.open(staticCacheName).then(cache => {
-                cache.match(e.request).then(cachedFile => {
-                    console.log('Файл найден в кэше', cachedFile)
-                    return cachedFile
-                }).catch(error => {
-                    console.warn('Файл в кэше не найден', error)
-                })
+function tryNetwork(req, timeout){
+    console.log(req)
+    return new Promise((resolve, reject) => {
+        const timeoutId = setTimeout(reject, timeout)
+        fetch(req).then((res) => {
+            clearTimeout(timeoutId)
+            const responseClone = res.clone()
+            caches.open(staticCacheName).then((cache) => {
+                cache.put(req, responseClone)
             })
-        }
-    })())
+            resolve(res)
+        }, reject)
+    })
+}
+
+function getFromCache(req){
+    console.log('Сеть плоха, работа с кэшем')
+    return caches.open(staticCacheName).then((cache) => {
+        return cache.match(req).then((result) => {
+            return result || Promise.reject("no-match")
+        })
+    })
+}
+
+self.addEventListener('fetch', (e)=>{
+    e.respondWith(tryNetwork(e.request, 400).catch(()=> getFromCache(e.request)))
 })
